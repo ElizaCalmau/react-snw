885e94a818eff6b01f88f9d2e03ccb66acccf4c6
ElizaCalmau <elizabethcalmau@gmail.com>
2022-05-30 19:08:37 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Page1 from './components/Page1';
+
+
+
+
+const App = () => {
+  return (
+    <div className='app_wrapper'>
+     <Header />
+     <Navbar />
+     <Page1 />
+    </div>
+  )
+}
+export default App;
---
+++ src/components/Page1.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Page1 = () => {
+    return(
+        <div className='page1'>
+       <div>
+         <img src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'/>
+       </div>
+       <div>
+         ava + description
+       </div>
+       <div>
+         my posts
+         <div>
+           new post
+         </div>
+       <div>
+         post1
+         </div>
+         <div>
+           post2
+         </div>
+       </div>
+     </div>
+    )
+}
+
+export default Page1;
