929090280880b957c0cb2ce6a69f89bedef2df45
ElizaCalmau <elizabethcalmau@gmail.com>
2022-05-30 20:09:37 +0300

    Fixed something

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import Page1 from './components/Page1';
 
 
 
+
 const App = () => {
   return (
     <div className='app_wrapper'>
      <Header />
      <Navbar />
      <Page1 />
+     
     </div>
   )
 }
