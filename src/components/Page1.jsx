6382d30a465d7d3ca2f4bdfcc26d6bc401318860
ElizaCalmau <elizabethcalmau@gmail.com>
2022-06-01 11:54:24 +0300

    Added css modules

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import s from './Page1.module.css';
 const Page1 = () => {
     return(
-        <div className='page1'>
+        <div className={s.page1}>
        <div>
          <img src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'/>
        </div>
@@ -14,12 +14,14 @@ const Page1 = () => {
          <div>
            new post
          </div>
-       <div>
+      <div className={s.posts}>
+       <div className={s.item}>
          post1
          </div>
-         <div>
+         <div className={s.item}>
            post2
          </div>
+       </div>
        </div>
      </div>
     )
