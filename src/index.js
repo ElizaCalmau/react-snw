f71f3e975fc338685757b3e3649c6a4e9fa6002e
ElizaCalmau <elizabethcalmau@gmail.com>
2022-06-20 12:40:50 +0300

    Put data from state to store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 
 import reportWebVitals from './reportWebVitals';
-import {state} from './redux/state' ;
+import store from './redux/state' ;
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import {addPost} from './redux/state';
-import { updateNewPostText } from './redux/state';
-import { updateNewTextMessage } from './redux/state';
-import {sendMessage} from './redux/state'; 
-import { subscribe } from './redux/state';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -20,16 +16,17 @@ let rerenderEntireTree = () => {
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App post={state.profilePage.post} 
-      friendsData={state.messagesPage.friendsData} 
-      messageData={state.messagesPage.messageData}
-      myFriends={state.sidebar.myFriends}
-      addPost = {addPost}
-      newPostText ={state.profilePage.newPostText}
-      updateNewPostText={updateNewPostText}
-      newTextMessage={state.messagesPage.newTextMessage}
-      updateNewTextMessage={updateNewTextMessage}
-      sendMessage={sendMessage}/>
+    <App post={store.getState().profilePage.post} 
+      friendsData={store.getState().messagesPage.friendsData} 
+      messageData={store.getState().messagesPage.messageData}
+      myFriends={store.getState().sidebar.myFriends}
+      newPostText={store.getState().profilePage.newPostText}
+      newTextMessage={store.getState().messagesPage.newTextMessage}
+      
+      addPost = {store.addPost.bind(store)}
+      updateNewPostText={store.updateNewPostText.bind(store)}
+      updateNewTextMessage={store.updateNewTextMessage.bind(store)}
+      sendMessage={store.sendMessage.bind(store)}/>
     </BrowserRouter>
   </React.StrictMode>
 );
@@ -38,8 +35,8 @@ root.render(
 
 
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,75 +1,87 @@
-let rerenderEntireTree = () => {
+let store={
+    _state: {
 
-}
+        profilePage: {
+    
+            post:[
+                {message:'hello, world!', likesCount: 10},
+                {message: 'It is me!', likesCount: 22},
+                {message: 'okay', likesCount: 25}
+            ],
+            newPostText:'it-kamasutra'
+        },
+        messagesPage: {
+    
+            friendsData:[
+                {nick: 'Liza', id: 1},
+                {nick: 'Andrew', id: 2},
+                {nick: 'Mosya', id: 3}
+            ],
+    
+            messageData:[
+                {text: 'Hi', id: 1},
+                {text: 'Wanna beer?', id: 2},
+                {text: 'Woof', id: 3}
+            ],
+            newTextMessage:'it-kamasutra'
+        },
+    
+        sidebar: {
+            myFriends:[
+               {name: 'Liza'} ,
+               {name: 'Andrew'},
+               {name: 'Mosya'}
+            ]
+           },
+    
+        },
+        getState(){
+            return this._state;
+        },
 
-export let state = {
+    _callSubscriber() {
+            console.log('State changed');
+        },
 
-    profilePage: {
-
-        post:[
-            {message:'hello, world!', likesCount: 10},
-            {message: 'It is me!', likesCount: 22},
-            {message: 'okay', likesCount: 25}
-        ],
-        newPostText:'it-kamasutra'
-    },
-    messagesPage: {
-
-        friendsData:[
-            {nick: 'Liza', id: 1},
-            {nick: 'Andrew', id: 2},
-            {nick: 'Mosya', id: 3}
-        ],
-
-        messageData:[
-            {text: 'Hi', id: 1},
-            {text: 'Wanna beer?', id: 2},
-            {text: 'Woof', id: 3}
-        ],
-        newTextMessage:'it-kamasutra'
-    },
-
-    sidebar: {
-        myFriends:[
-           {name: 'Liza'} ,
-           {name: 'Andrew'},
-           {name: 'Mosya'}
-        ]
-       },
-
-    }
-    export let addPost = () => {
+    addPost() {
       
-        let newPost = {
-            id: 5,
-            message: state.profilePage.newPostText,
-            likesCount: 0
-        };
-        state.profilePage.post.push(newPost);
-        state.profilePage.newPostText='';
-        rerenderEntireTree(state);
-    }
-    export let updateNewPostText = (newText) => {
-        state.profilePage.newPostText = newText;
-        
-        rerenderEntireTree(state);
-    }
-    export let sendMessage = () => {
-    let newMessage = {
-        text: state.messagesPage.newTextMessage,
-        id: 4
-    }
-    state.messagesPage.messageData.push(newMessage);
-    state.messagesPage.newTextMessage=''
-    rerenderEntireTree(state);
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0
+            };
+            this._state.profilePage.post.push(newPost);
+            this._state.profilePage.newPostText='';
+            this._callSubscriber(this._state);
+        },
+
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+            
+        this._callSubscriber(this._state);
+        },
+
+    sendMessage () {
+            let newMessage = {
+                text: this._state.messagesPage.newTextMessage,
+                id: 4
+            }
+            this._state.messagesPage.messageData.push(newMessage);
+            this._state.messagesPage.newTextMessage=''
+            this._callSubscriber(this._state);
+        },
+
+    updateNewTextMessage (newMessage) {
+        this._state.messagesPage.newTextMessage = newMessage;//?
+        this._callSubscriber(this._state)
+        },
+    subscribe (observer){
+        this._callSubscriber = observer;
+        }
 }
 
-    export let updateNewTextMessage = (newMessage) => {
-        state.messagesPage.newTextMessage = newMessage;//?
-        rerenderEntireTree(state)
-    } 
-    export let subscribe = (observer) => {
-        rerenderEntireTree = observer;
-    }
-    window.state = state
+
+ 
+    export default store;
+    window.store = store;
 
